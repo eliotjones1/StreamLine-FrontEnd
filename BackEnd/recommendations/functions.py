94bbcad000934d9e3a0c5ce2287489b8f33f19f0
Eliot Jones <eliot.k.jones@gmail.com>
2023-06-13 13:47:11 -0400

    recommendation system scaffolding

---
+++ BackEnd/recommendations/functions.py
@@ -0,0 +1,150 @@
+import requests
+import json
+from rest_framework.response import Response
+from rest_framework import generics, status
+import pandas as pd
+import numpy as np
+from .models import *
+from .serializers import *
+from surprise import Reader, Dataset, SVD
+from user_auth.models import CustomUser
+
+DATA_HAS_BEEN_GENERATED = 0
+
+def returnData(target_user_id):
+    cur_ratings = MediaRatings.objects.all().values()
+    df = pd.DataFrame.from_records(cur_ratings)
+    df = df.drop(columns = ['id', 'media_type'])
+
+    reader = Reader(rating_scale=(1, 5))
+
+    data = Dataset.load_from_df(df[["media_id", "rating", "user_id"]], reader)
+    trainset = data.build_full_trainset()
+
+    algo = SVD()
+    algo.fit(trainset)
+
+    target_inner_id = trainset.to_inner_uid(target_user_id)
+    similar_users = algo.get_neighbors(target_inner_id, 5)
+    similar_user_ids = [trainset.to_raw_uid(inner_id) for inner_id in similar_users]
+
+    recommendation_ids = []
+    for user_id in similar_user_ids:
+        user_recs = algo.get_top_n(trainset.ur[trainset.to_inner_uid(user_id)], n=5)
+        for rec in user_recs:
+            recommendation_ids.extend(rec[0])
+    recs = []
+    for id in recommendation_ids:
+        if MediaRatings.objects.filter(media_id = id, user_id = target_user_id).exists():
+            recommendation_ids.remove(id)
+        if MediaRatings.objects.get(media_id = id, user_id = 1).type == "tv":
+            url = "https://api.themoviedb.org/3/tv/" + str(id) + "?language=en-US"
+
+            headers = {
+                "accept": "application/json",
+                "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NWNkNTI3OWYxN2M2NTkzMTIzYzcyZDA0ZTBiZWRmYSIsInN1YiI6IjY0NDg4NTgzMmZkZWM2MDU3M2EwYjk3MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VXG36aVRaprnsBeXXhjGq6RmRRoPibEuGsjkgSB-Q-c"
+            }
+
+            response = requests.get(url, headers=headers)
+            info = response.json()
+            info['type'] = "tv"
+            recs.append(info)
+        else:
+            url = "https://api.themoviedb.org/3/movie/" + str(id) + "?language=en-US"
+            headers = {
+                "accept": "application/json",
+                "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NWNkNTI3OWYxN2M2NTkzMTIzYzcyZDA0ZTBiZWRmYSIsInN1YiI6IjY0NDg4NTgzMmZkZWM2MDU3M2EwYjk3MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VXG36aVRaprnsBeXXhjGq6RmRRoPibEuGsjkgSB-Q-c"
+            }
+            response = requests.get(url, headers=headers)
+            info = response.json()
+            info['type'] = "movie"
+            recs.append(info)
+    
+    return recs
+        
+
+def generateData(page_input):
+    if DATA_HAS_BEEN_GENERATED == 1:
+        print("Data has been generated already")
+        return 0
+    else:
+        #get all users
+        users = CustomUser.objects.all()
+        #get all media
+
+        # Get popular movies from TMDB
+
+        url = "https://api.themoviedb.org/3/movie/popular?language=en-US&page=" + page_input
+        print(url)
+
+        headers = {
+                    "accept": "application/json",
+                     "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NWNkNTI3OWYxN2M2NTkzMTIzYzcyZDA0ZTBiZWRmYSIsInN1YiI6IjY0NDg4NTgzMmZkZWM2MDU3M2EwYjk3MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VXG36aVRaprnsBeXXhjGq6RmRRoPibEuGsjkgSB-Q-c"
+                }
+
+        response_movies = requests.get(url, headers=headers)
+
+
+        url = "https://api.themoviedb.org/3/tv/popular?language=en-US&page=" + page_input
+
+        headers = {
+                    "accept": "application/json",
+                    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NWNkNTI3OWYxN2M2NTkzMTIzYzcyZDA0ZTBiZWRmYSIsInN1YiI6IjY0NDg4NTgzMmZkZWM2MDU3M2EwYjk3MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VXG36aVRaprnsBeXXhjGq6RmRRoPibEuGsjkgSB-Q-c"
+                }
+
+        response_tv = requests.get(url, headers=headers)
+        print(response_movies.status_code)
+        movie_pop = response_movies.json()
+        tv_pop = response_tv.json()
+
+        top_thousand_movies = movie_pop['results'][0:1000]
+        top_thousand_tv = tv_pop['results'][0:1000]
+
+        # Create a list of all the media
+        media_list = top_thousand_movies + top_thousand_tv
+
+        # For each entry in media_list
+        # for each user in the database
+        # Generate a random number between 1 and 10 from normal distribution centered around vote_average of media
+        # Create a new MediaRatings entry with the user, media, and rating
+        # Save the entry
+        for entry in media_list:
+            for user in users:
+                # Generate a random number between 1 and 10 from normal distribution centered around vote_average of media
+                rating = np.random.normal(entry['vote_average'], 1)
+                # Create a new MediaRatings entry with the user, media, and rating
+                if MediaRatings.objects.filter(media_id = entry['id'], user_id = user.to_json()).exists():
+                    cur = MediaRatings.objects.get(media_id = entry['id'], user_id = user.to_json())
+                    if entry in top_thousand_movies:
+                        cur.media_type = "movie"
+                    else:
+                        cur.media_type = "tv"
+                    cur.save()
+                    continue
+
+                new = MediaRatings()
+                new.media_id = entry['id']
+                new.user_id = user.to_json()
+                new.rating = rating
+                if entry in top_thousand_movies:
+                    new.media_type = "movie"
+                else:
+                    new.media_type = "tv"
+                new.media_type = entry['media_type']
+                # Save the entry
+                new.save()
+            if 'title' in entry:
+                print(entry['title'] + " was a success!")
+            else:
+                print(entry['name'] + " was a success!")
+        return 1
+        
+
+
+
+        
+
+
+            
+        
+
---
+++ BackEnd/recommendations/views.py
@@ -0,0 +1,73 @@
+import requests
+from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .functions import *
+import cvxpy as cp
+import numpy as np
+import pandas as pd
+from django.core.cache import cache
+from user_auth.models import *
+from user_auth.serializers import *
+from .models import *
+from .serializers import *
+from surprise import Reader, Dataset
+from .functions import *
+
+
+@api_view(['POST'])
+def saveRating(request):
+    data = request.data
+    # Expect email to be at 0
+    # Expect object at 1, rating at 2
+    # Functionality: When the user, in their dashboard, removes something from their list, we prompt them to rate it. 
+    # This rating corresponds to the TMDB ID of the media, and gets saved to their account. Have users rate by choosing a number 1-10.
+    # Maybe prompt them to rate some stuff in the beginning, and then have them rate media as they remove them from their list.
+    user = data[0]
+    object_id = data[1]
+    rating = data[2]
+    user_exists = CustomUser.objects.get(email = user)
+
+    # If there is a MediaRatings entry with the same object_id and user, we want to override it with new information. 
+    # Else, create a new entry
+    if MediaRatings.objects.filter(media = object_id, user = user_exists).exists():
+        current = MediaRatings.objects.get(media = object_id, user = user_exists)
+        current.rating = rating
+        current.save()
+    else:
+        new = MediaRatings(media = object_id, user = user_exists, rating = rating)
+        new.save()
+
+    return Response({"Status":"OK"})
+
+
+class returnRecommendations(generics.ListAPIView):
+     def get(self, request):
+        query = request.query_params.get('email', None)
+        if query is None:
+            return Response({'error': 'Missing search query'}, status=status.HTTP_400_BAD_REQUEST)
+        email = query
+        user_exists = CustomUser.objects.get(email = email)
+        # If user doesn't exist throw an error
+        if not user_exists:
+            return Response({'error': 'User does not exist'}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            recs = returnData(user_exists.id)
+            sorted_recs = sorted(recs, key = lambda k: k['vote_average'], reverse = True)
+            top_five = sorted_recs[:5]
+            return Response(top_five, status=status.HTTP_200_OK)
+
+
+class generate(generics.ListAPIView):
+    def get(self, request):
+        query = request.query_params.get('generate', None)
+        if query is None:
+            return Response({'error': 'Missing search query'}, status=status.HTTP_400_BAD_REQUEST)
+        elif query == '1':
+            for i in range(1,29):
+                page_input = str(i)
+                generateData(page_input)
+            return Response({'Status': 'OK'}, status=status.HTTP_200_OK)
+        else:
+            return Response({'error': 'Query set to 0'}, status=status.HTTP_400_BAD_REQUEST)
