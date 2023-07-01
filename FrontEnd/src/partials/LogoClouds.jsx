55cbacbd885ac022ad8f1634bbc7cecc296fff92
Ryan Dunn <rcdunn01@stanford.edu>
2023-07-01 18:16:40 -0400

    dark mode bug fixed

--- FrontEnd/src/partials/LogoClouds.jsx
+++ FrontEnd/src/partials/LogoClouds.jsx
@@ -1,12 +1,6 @@
 import React from "react";
 import { useNavigate } from 'react-router-dom';
 
-import Netflix from '../images/Netflix_Logo.png'
-import DisneyPlus from '../images/Disney_Plus_Logo.png'
-import AmazonPrimeVideo from '../images/Amazon_Prime_Video_Logo.png'
-import HBOMax from "../images/HBO_Max_Logo.png"
-import Hulu from "../images/Hulu_Logo.png"
-
 function LogoClouds() {
   const nav = useNavigate();
 
@@ -18,39 +12,54 @@ function LogoClouds() {
         <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
           <img
             className="col-span-2 max-h-18 w-full object-contain lg:col-span-1"
-            src={Hulu}
+            src="https://1000logos.net/wp-content/uploads/2020/12/Hulu-Logo-PNG.png"
             alt="Hulu"
             width={158}
             height={48}
           />
           <img
-            className="col-span-2 max-h-18 w-full object-contain lg:col-span-1"
-            src={DisneyPlus}
+            className="col-span-2 max-h-18 w-full object-contain lg:col-span-1 hidden dark:block"
+            src="https://1000logos.net/wp-content/uploads/2021/01/Disney_Plus_logo_PNG5.png"
             alt="Disney+"
             width={158}
             height={48}
           />
           <img
-            className="col-span-2 max-h-18 w-full object-contain lg:col-span-1"
-            src={Netflix}
+            className="col-span-2 max-h-18 w-full object-contain lg:col-span-1 dark:hidden"
+            src="https://1000logos.net/wp-content/uploads/2023/01/Disney-Plus-logo.png"
+            alt="Disney+"
+            width={158}
+            height={48}
+          />
+          <img
+            className="col-span-2 max-h-18 w-full object-contain lg:col-span-1 block"
+            src="https://1000logos.net/wp-content/uploads/2017/05/Netflix-Logo.png"
             alt="Netflix"
             width={158}
             height={48}
           />
           <img
-            className="col-span-2 max-h-18 w-full object-contain sm:col-start-2 lg:col-span-1"
-            src={AmazonPrimeVideo}
+            className="col-span-2 max-h-18 w-full object-contain sm:col-start-2 lg:col-span-1 block"
+            src="https://1000logos.net/wp-content/uploads/2022/10/Amazon_Prime_Video_Logo_PNG3.png"
             alt="Amazon Prime Video"
             width={158}
             height={48}
           />
           <img
-            className="col-span-2 col-start-2 max-h-18 w-full object-contain sm:col-start-auto lg:col-span-1"
-            src={HBOMax}
+            className="col-span-2 col-start-2 max-h-18 w-full object-contain sm:col-start-auto lg:col-span-1 block dark:hidden"
+            src="https://1000logos.net/wp-content/uploads/2022/02/HBO-Max-Logo.png"
             alt="HBO Max"
             width={158}
             height={48}
           />
+          <img
+            className="col-span-2 col-start-2 max-h-18 w-full object-contain sm:col-start-auto lg:col-span-1 hidden dark:block filter invert"
+            src="https://1000logos.net/wp-content/uploads/2022/02/HBO-Max-Logo-PNG1.png"
+            alt="HBO Max"
+            width={158}
+            height={48}
+          />
+          
         </div>
         
         <div className="flex flex-col items-center w-full pt-10">
--- FrontEnd/src/partials/PageSelection.jsx
+++ FrontEnd/src/partials/PageSelection.jsx
@@ -10,20 +10,6 @@ export default function Example({baseIndex, changeBaseIndex, articlesList, numPe
 
   return (
     <div className="mx-auto max-w-7xl flex items-center justify-between border-t border-gray-200 px-4 py-3 sm:px-6">
-      <div className="flex flex-1 justify-between sm:hidden">
-        <a
-          href="#"
-          className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
-        >
-          Previous
-        </a>
-        <a
-          href="#"
-          className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
-        >
-          Next
-        </a>
-      </div>
       <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
         <div>
           <p className="text-sm text-gray-700 dark:text-white">
