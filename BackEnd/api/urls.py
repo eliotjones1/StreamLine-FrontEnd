14d1921b1f51e6ee4e465228c436692e7b34193a
Eliot Jones <eliot.k.jones@gmail.com>
2023-07-15 11:28:29 -0400

    fix

--- BackEnd/api/urls.py
+++ BackEnd/api/urls.py
@@ -16,5 +16,5 @@ urlpatterns = [
     path('returnInfo/', returnInfo, name="return_info"),
     path('recent/', newlyReleased.as_view(), name="recent"),
     path('popularServices/', popularServices.as_view(), name="popularServices"),
-    path('/isAuthenticated/', isAuthenticated, name='isAuthenticated'),
+    path('isAuthenticated/', isAuthenticated.as_view(), name='isAuthenticated'),
 ]
