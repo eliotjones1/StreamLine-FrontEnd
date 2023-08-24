18c5504c0bfccc35b906e6eb34273a3c789fd053
Ryan Dunn <rcdunn01@stanford.edu>
2023-08-23 21:55:12 -0700

    stuff is broken

---
+++ FrontEnd/src/modules/account/pages/content/components/Featured/components/Video/index.jsx
@@ -0,0 +1,9 @@
+export default function VideoBackdrop() {
+	const featuredMediaTrailer =
+		'http://www.youtube.com/embed/J_1EXWNETiI?autoplay=1&controls=0&showinfo=0';
+	return (
+		<video>
+			<source src="" />
+		</video>
+	);
+}
