d5889385b15aa29aabb782278380662f62185f6d
Eliot Jones <eliot.k.jones@gmail.com>
2023-09-18 15:16:36 -0700

    i

--- src/api/modules/auth/index.jsx
+++ src/api/modules/auth/index.jsx
@@ -45,18 +45,18 @@ export default function LoginProvider({ children }) {
 	};
 
 	const signUp = async (userData) => {
-		await StreamLineAxios.post('/auth/register/', userData);
+		await StreamLineAxios.post('/auth/auth/register/', userData);
 		login(userData);
 	};
 
 	const login = async (userData) => {
-		await StreamLineAxios.post('/auth/login/', userData);
+		await StreamLineAxios.post('/auth/auth/login/', userData);
 		dispatch({ type: 'LOGIN', payload: { isAdmin: false } });
 		nav('/user-dash');
 	};
 
 	const logout = async () => {
-		await StreamLineAxios.post('/auth/logout/', {});
+		await StreamLineAxios.post('/auth/auth/logout/', {});
 		dispatch({ type: 'LOGOUT' });
 		nav('/');
 	};
